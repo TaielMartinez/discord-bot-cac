7c40906ee60060c840ddf4c60f6e0e15f3ecb04d
tataicapo <tataicapo@gmail.com>
2019-02-22 02:49:45 -0300

    Limpie el código

--- index.js
+++ index.js
@@ -3,61 +3,35 @@ const client = new Discord.Client();
 
 client.login("NTQ3OTU4NjU3NDE0MDcwMjc1.D0-V-A.i1KiB8_Qvvjm1Iva0QZ23tdNT1I");
 
-  
-     
-		  var rowes;
-  
 var GoogleSpreadsheet = require('google-spreadsheet');
-
 var creds = require('./client_secret.json');
 
- 
-
-// Create a document object using the ID of the spreadsheet - obtained from its URL.
+var rowes;
 
 var doc = new GoogleSpreadsheet('1XVJb2TxkEAhM2jH-t8NMjV1NNas7cQQcJW7soVpGPP0');
 
 
-
-
-
-// Authenticate with the Google Spreadsheets API.
-
 doc.useServiceAccountAuth(creds, function (err) {
 
- 
+	doc.getRows(1, function (err, rows) {
 
-  // Get all of the rows from the spreadsheet.
+		rowes=rows;
 
-  doc.getRows(1, function (err, rows) {
-rowes=rows;
-
-  });
-
+	});
 });
 
 
-
-
-
-	client.on('message', msg => {
+client.on('message', msg => {	
 		
+	if(msg.member.user.tag != "Bot de Prueba#6012"){
 
+		for (i = 0; i < rowes.length; i+=4) {
 
-		
-		
-	if(msg.member.user.tag != "Bot de Prueba#6012")
-	{
-for (i = 0; i < rowes.length; i+=4) {
-	if (msg.content.includes(rowes[i].leer))
-	{
-		msg.reply(rowes[i+2].leer);
-	}
-}
-	
-		
-	
-};
+			if (msg.content.includes(rowes[i].leer)){
 
-  });
+				msg.reply(rowes[i+2].leer);
+			}
+		}
+	};
+});
   
