45fb0b1b9874a43d8ceaf61b543f573943c08ec8
tataicapo <tataicapo@gmail.com>
2019-02-22 02:43:59 -0300

    Utilización de google sheet como DB

--- index.js
+++ index.js
@@ -3,8 +3,61 @@ const client = new Discord.Client();
 
 client.login("NTQ3OTU4NjU3NDE0MDcwMjc1.D0-V-A.i1KiB8_Qvvjm1Iva0QZ23tdNT1I");
 
-client.on('message', msg => {
-    if (msg.content === 'ah, por cierto luk, el bot') {
-      msg.reply('funciona!!!!');
-    }
+  
+     
+		  var rowes;
+  
+var GoogleSpreadsheet = require('google-spreadsheet');
+
+var creds = require('./client_secret.json');
+
+ 
+
+// Create a document object using the ID of the spreadsheet - obtained from its URL.
+
+var doc = new GoogleSpreadsheet('1XVJb2TxkEAhM2jH-t8NMjV1NNas7cQQcJW7soVpGPP0');
+
+
+
+
+
+// Authenticate with the Google Spreadsheets API.
+
+doc.useServiceAccountAuth(creds, function (err) {
+
+ 
+
+  // Get all of the rows from the spreadsheet.
+
+  doc.getRows(1, function (err, rows) {
+rowes=rows;
+
   });
+
+});
+
+
+
+
+
+	client.on('message', msg => {
+		
+
+
+		
+		
+	if(msg.member.user.tag != "Bot de Prueba#6012")
+	{
+for (i = 0; i < rowes.length; i+=4) {
+	if (msg.content.includes(rowes[i].leer))
+	{
+		msg.reply(rowes[i+2].leer);
+	}
+}
+	
+		
+	
+};
+
+  });
+  
